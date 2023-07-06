c7259dd78ed474a249b438bf13a6a50c1e5bd4ef
Giacomo Fabbian <giacomo.fabbian@gmail.com>
2023-07-06 17:27:56 +0200

    Update HereGeocoderFunctionTest.php

--- tests/HereGeocoderFunctionTest.php
+++ tests/HereGeocoderFunctionTest.php
@@ -11,7 +11,7 @@ class HereGeocoderFunctionTest extends TestCase
 
     protected $hereGeocoder;
 
-    protected function setUp(): void
+    public function setUp(): void
     {
         parent::setUp();
         $this->hereGeocoder = new HereGeocoder();
